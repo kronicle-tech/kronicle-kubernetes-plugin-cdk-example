999e8301def5ad11a65b6afb23feb61a523f3cb2
Simon Dean <simon@simondean.org>
2022-07-08 12:24:37 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kronicle Kubernetes Plugin CDK Example
+
+An example AWS EKS Kubernetes cluster, created with AWS CDK, to test and demo the Kubernetes plugin for Kronicle.
---
+++ index.ts
@@ -0,0 +1,57 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as eks from 'aws-cdk-lib/aws-eks';
+
+export class EksExampleStack extends cdk.Stack {
+  private vpc: ec2.IVpc;
+  private eksCluster: eks.Cluster;
+
+  constructor(app: cdk.App, id: string, props?: cdk.StackProps) {
+    super(app, id, props);
+
+    this.createVpc();
+    this.createEksCluster();
+    this.applyHelmChart();
+  }
+
+  private createVpc() {
+    this.vpc = ec2.Vpc.fromLookup(this, "Vpc", {
+      vpcName: "kronicle",
+    });
+  }
+
+  private createEksCluster() {
+    this.eksCluster = new eks.Cluster(this, 'EksCluster', {
+      vpc: this.vpc,
+      version: eks.KubernetesVersion.V1_22,
+      clusterName: 'kronicle-kubernetes-plugin-example',
+      vpcSubnets: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC,
+          onePerAz: true,
+        }
+      ]
+    });
+  }
+
+  private applyHelmChart() {
+    this.eksCluster.addHelmChart('ArgoCd', {
+      chart: 'argo-cd',
+      version: '4.9.8',
+      repository: 'https://argoproj.github.io/argo-helm',
+      release: 'arco-cd'
+    });
+  }
+}
+
+const app = new cdk.App();
+new EksExampleStack(app, 'EksExample', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
+cdk.Tags.of(app).add('team', 'kronicle-project');
+cdk.Tags.of(app).add('component', 'kronicle-kubernetes-plugin-example');
+cdk.Tags.of(app).add('example', 'true');
+app.synth();
