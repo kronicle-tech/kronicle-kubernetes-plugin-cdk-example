0885e3e330772cfaf235ad2f96ba874704b2de13
Simon Dean <simon@simondean.org>
2022-07-13 00:00:17 +0100

    Get auth working (#18)

--- index.ts
+++ index.ts
@@ -65,7 +65,7 @@ export class EksExampleStack extends cdk.Stack {
       subjects: [
         {
           kind: 'User',
-          name: 'kubernetes-api-read-only',
+          name: 'kronicle-service',
           apiGroup: 'rbac.authorization.k8s.io',
         },
       ],
@@ -79,22 +79,10 @@ export class EksExampleStack extends cdk.Stack {
       cluster: this.eksCluster,
     });
     awsAuth.addAccount(this.account);
-    const kubernetesApiReadOnlyRole = new iam.Role(this, 'KubernetesApiReadOnlyRole', {
-      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-      inlinePolicies: {
-        kubernetesApi: new iam.PolicyDocument({
-          statements: [new iam.PolicyStatement({
-            actions: [
-              'eks:AccessKubernetesApi',
-            ],
-            resources: [this.eksCluster.clusterArn],
-          })],
-        })
-      }
-    });
-    awsAuth.addRoleMapping(kubernetesApiReadOnlyRole, {
-      username: 'kubernetes-api-read-only',
-      groups: ['system:discovery', 'api-read-only'],
+    const kronicleServiceRole = iam.Role.fromRoleName(this, 'KronicleServiceRole', 'KronicleStack-KronicleTaskDefinitionTaskRoleCBE6C8-1SR6QRA1JM9NB')
+    awsAuth.addRoleMapping(kronicleServiceRole, {
+      username: 'kronicle-service',
+      groups: ['system:discovery'],
     })
   }
 
